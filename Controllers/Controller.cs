1b441f05943a79ab524b22aaa4870682e063092e
Katelli <aska082024@gmail.com>
2024-10-08 09:55:35 +0200

    Added method to add a new game

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -8,13 +8,48 @@ public class Controller
         _model = model;
         _view = view;
     }
+
+    public void Run()
+    {
+
+    }
+    //          Todo list
+    // Add Game
+    private void AddGame()
+    {
+        Console.WriteLine("Enter the game's title/name");
+        string? title = Console.ReadLine();
+
+        while(string.IsNullOrEmpty(title))
+        {
+            Console.WriteLine("Title can not be empty");
+            title = Console.ReadLine();
+        }
+
+
+        Console.WriteLine("Enter the game's release year");
+
+        string? yearInput = Console.ReadLine();
+        int year;
+        while(!int.TryParse(yearInput, out year))
+        {
+            Console.WriteLine("There was an error with the data submitted, please input the year using numbers");
+            yearInput = Console.ReadLine();
+        }
+
+
+        Console.WriteLine("Enter the game's genre");
+        string? genre = Console.ReadLine();
+        while(string.IsNullOrEmpty(genre))
+        {
+            Console.WriteLine("Genre can not be empty");
+            genre = Console.ReadLine();
+        }
+
+    }
+
+    // Remove Game
+
+    // Update Title, Year or Genre
+
 }
-
-//          Todo list
-// Add Game
-
-// Remove Game
-
-// Update Title, Year or Genre
-
-// Add public Run() method
